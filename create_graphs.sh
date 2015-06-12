1385490f9761713b9eb92e53df2c9a32d049fa88
Katie Li <katiesli16@gmail.com>
2015-06-11 21:16:05 -0400

    Refactoring and updating

--- create_graphs.sh
+++ create_graphs.sh
@@ -1,7 +1,8 @@
 ## Creates 4 graphs: temperature min, temperature max, precipitation, and evapotranspiration
 mkdir graphs
-python3 simple_analysis.py data.csv Air_max 'Maximum Temperature'  Fahrenheit
-python3 simple_analysis.py data.csv Air_min 'Minimum Temperature'  Fahrenheit
-python3 simple_analysis.py data.csv Precip Precipitation inches 
-python3 simple_analysis.py data.csv "ETo" "Reference evapotranspiration  Inches" inches 
+python3 simple_analysis.py
+#python3 simple_analysis.py data.csv Air_max 'Maximum Temperature'  Fahrenheit
+#python3 simple_analysis.py data.csv Air_min 'Minimum Temperature'  Fahrenheit
+#python3 simple_analysis.py data.csv Precip Precipitation inches 
+#python3 simple_analysis.py data.csv "ETo" "Reference evapotranspiration  Inches" inches 
 
--- simple_analysis.py
+++ simple_analysis.py
@@ -8,35 +8,47 @@ import matplotlib.pyplot as plt
 from textwrap import wrap
 import sys
 
-## readin arguments from command line
-if len(sys.argv) > 3:
-  filename = sys.argv[1]
-  variable = sys.argv[2]
-  graphName = sys.argv[3]
-  if len(sys.argv) > 4: 
-    units = sys.argv[4] # units are optional
-  else:
-    units = ""
-else: 
-  raise Exception("Incorrect number of arguments")
-
+filename = input("data file to read from:" ) 
 DEFAULT_HEADER =60
 df = pd.read_csv(filename, header=DEFAULT_HEADER, delim_whitespace=False)
 
 f =  lambda u: int(str(u)[4:-2]) 
+f2 = lambda u: int(str(u)[:-4])
 df['month'] = df['Date'].map(f) 
+df['year'] = df['Date'].map(f2) 
+df['copy_month'] = df['month']
+df['copy_year'] = df['year']
 ## get rid of missing data for specified variable
-df2 = df[['Date', variable,'month']].dropna()
-df2['copy_month'] = df2['month']
-## average by monthly data
-df2 = df2.groupby('month').mean()
+
+def analyzeOneVariable(df, timeFrame, variable):
+  df2 = df[['Date', variable,timeFrame, 'copy_' + timeFrame]].dropna()
+  df2 = df2.groupby(timeFrame).mean()
+  df2['std_'+ variable] = df2[variable].std() 
+  df2[variable+ "_Ustd"] = df2[variable] + df2['std_'+ variable] 
+  df2[variable+ "_Dstd"] = df2[variable] - df2['std_'+ variable] 
+  return df2
 
 ## create simple, minimalistic plot
-plt.cla()
-plt.title("\n".join(wrap("Monthly mean for " + graphName + " 1989 - 2015,  Pomona California", 50)))
-plt.xlabel('Time (month)', fontsize=12, color='black')
-plt.ylabel(graphName+ " " +  units, fontsize = 12, color='black')
-plt.plot(df2['copy_month'], df2[variable], color="#078ccc", marker='o')
-plt.xlim([0.5,12.5])
-plt.savefig('graphs/'+ graphName + ".png")
-plt.cla()
+def createPlot(timeFrame, plotTitle, variable, units, df2):
+  plt.cla()
+  plt.title("\n".join(wrap(" Mean for each " + timeFrame + " , "+ plotTitle + \
+            " 1989 - 2015,  Pomona California", 50)))
+  plt.xlabel('Time '+ timeFrame, fontsize=12, color='black')
+  plt.ylabel(plotTitle+ " " +  units, fontsize = 12, color='black')
+  plt.plot(df2['copy_' + timeFrame], df2[variable], color="#078ccc", marker='o')
+  plt.plot(df2['copy_' + timeFrame], df2[variable +"_Ustd"], color="#000000")
+  plt.plot(df2['copy_' + timeFrame], df2[variable + "_Dstd"], color="#000000")
+  print(df2[[str('copy_'+ timeFrame),variable]])
+  print(df2.index)
+  plt.savefig('graphs/'+ plotTitle+ timeFrame + ".png")
+  plt.cla()
+
+if __name__ == '__main__': 
+  df2 = analyzeOneVariable(df, "month", "Precip")
+  createPlot("month", "Precipitation", "Precip", "", df2) 
+  df2.to_csv("csv/MonthlyPrecip.csv", encoding='utf-8')
+
+  df2 = analyzeOneVariable(df, "year", "Precip")
+  createPlot("year", "Precipitation", "Precip", "", df2) 
+  df2.to_csv("csv/YearlyPrecip.csv", encoding='utf-8')
+  print("Done")
